8336f1dd47c7af97787b67c20ae63ee6f3c5f915
Arafat M <github@arafatm.com>
2017-07-15 15:08:15 -0700

    naive worker implementation

--- metex/test/worker_test.exs
+++ metex/test/worker_test.exs
@@ -10,4 +10,13 @@ defmodule Metex.WorkerTest do
     temp = Metex.Worker.temperature_of("Omicron Persei 8")
     assert String.starts_with?(temp, "Omicron Persei 8 not found")
   end
+
+  test "loop/0 over multiple locations" do
+    pid = spawn(Metex.Worker, :loop, [])
+    cities = ["Singapore", "Monaco", "Vatican City", "Hong Kong", "Macau"]
+    cities |> Enum.each(fn city ->
+      send(pid, {self(), city})
+    end)
+    # TODO: how do i check results here?
+  end
 end                                                                                                                                                      
